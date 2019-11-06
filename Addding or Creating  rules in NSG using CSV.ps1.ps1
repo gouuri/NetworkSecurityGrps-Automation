42e7f700b3d98584bb17be44b531ae6b3ac13317
varunthejdev <43825625+varunthejdev@users.noreply.github.com>
2019-11-06 08:04:11 +0000

    Add files via upload

---
+++ Addding or Creating  rules in NSG using CSV.ps1.ps1
@@ -0,0 +1,14 @@
+﻿$nsgname = "vm-rgpb1-nsg"
+$resourceGroupName = "rgp-b"
+$NSG = Get-AzureRmNetworkSecurityGroup -Name $nsgname`
+                 -ResourceGroupName $resourceGroupName
+
+foreach($rule in import-csv "inputfile.csv")
+{ 
+    $NSG | add-AzureRmNetworkSecurityRuleConfig -Name $rule.name `
+           -Access Allow -Protocol Tcp -Direction $rule.direction -Priority $rule.priority `
+           -SourceAddressPrefix $rule.source -SourcePortRange * `
+           -DestinationAddressPrefix $rule.destination -DestinationPortRange $rule.port
+}
+
+$NSG | Set-AzureRmNetworkSecurityGroup
